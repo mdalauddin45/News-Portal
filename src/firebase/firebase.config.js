4eb7a9e70fcfc96652ae123e40036a12ddcc24fe
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-18 10:01:53 +0600

    fgds

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvwHC-F87NTaUxBM0OFKA8iPgBDJ6-S_w",
+  authDomain: "bgc-news.firebaseapp.com",
+  projectId: "bgc-news",
+  storageBucket: "bgc-news.appspot.com",
+  messagingSenderId: "88451870363",
+  appId: "1:88451870363:web:34180e5d57773acc56203e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
