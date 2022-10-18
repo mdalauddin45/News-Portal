9fac9d3eda70d78c1f2df26025d9410e31bf0e8c
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-18 10:09:00 +0600

    main

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../../components/Home/Home";
+import Main from "../../layout/Main";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home></Home>,
+    element: <Main></Main>,
+    children: [{ path: "/", element: <Home></Home> }],
   },
 ]);
