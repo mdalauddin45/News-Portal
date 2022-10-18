70b0e908ead05ce8ab57189aab48921a5d314dbc
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-18 10:10:19 +0600

    Header add

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Home from "../components/Home/Home";
+import Header from "../components/Header/Header";
 
 const Main = () => {
   return (
     <div>
-      <Home></Home>
+      <Header></Header>
       <Outlet></Outlet>
     </div>
   );
