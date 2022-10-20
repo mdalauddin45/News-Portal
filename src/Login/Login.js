1d8be213f23d1b390334553d142e5f683781a4f9
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-20 20:54:03 +0600

    reset

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -11,10 +11,13 @@ const Login = () => {
     const form = event.target;
     const email = form.email.value;
     const password = form.password.value;
-    logInUser(email, password).then((result) => {
-      const user = result.user;
-      console.log(user).catch((error) => console.error(error));
-    });
+    logInUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+      })
+      .catch((error) => console.error(error));
   };
 
   return (
